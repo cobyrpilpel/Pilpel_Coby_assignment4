d124f33e492d8e859b1f0ac3888a55e33467351d
cobyrpilpel <cobyrpilpel@Cobys-MacBook-Pro.local>
2014-08-28 18:48:43 -0400

    string function 1 complete

--- rename-me.js
+++ rename-me.js
@@ -6,9 +6,12 @@
 //Aug 28th 2014
 
 //Global variables (I'm throwing these up here as I run into a situation where I need one)
-//Number function 1 global variables
-//none needed
-//Number function 2 global variables
+var checkUrl = "https://orgsync.com"
+//var checkUrl = "htts://orgsync.com" <----Alternate incorrectly formatted URL to confirm string function 1 works.
+
+
+
+
 
 //Number function 1 - Converting a string to an integer. Prompt for the number active.
 //Code also works with the included simpler variable that I commented into the code.
@@ -25,9 +28,20 @@ console.log(convertToNumber())
 console.log(isNaN(convertToNumber))
 
 //Number function 2 - Format a number to use a specific number of decimal places
+//brain block - will come back to this later
 
 
 
+//String function 1 - Is the string a URL?
+var urlCheck = function(checkUrl) 
+	{var url ;
+	if(checkUrl.substring(0,7) === "http://" || checkUrl.substring(0,8) === "https://") 
+	{url = true;} 
+	else 
+	{url = false;}
+    return url;};
 
+console.log(urlCheck(checkUrl))
 
+//
 
