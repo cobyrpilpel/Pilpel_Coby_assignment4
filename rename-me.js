e123c550e155bd9c83850cb8af517f5ebf292639
cobyrpilpel <cobyrpilpel@Cobys-MacBook-Pro.local>
2014-08-28 17:14:39 -0400

    math function 1 complete
    got my first math function done. To be honest I ran into so many syntax
    errors putting that small function together I’m not even 100% sure why
    it worked in that incarnation. I’m going to look over it a few more
    times and read back in the class material to figure out why it works
    now and not on my earlier tries.

--- rename-me.js
+++ rename-me.js
@@ -4,3 +4,23 @@
 // Project 4
 //SDI 1408
 //Aug 28th 2014
+
+//Global variables (I'm throwing these up here as I run into a situation where I need one)
+
+
+//Number function 1 - Converting a string to an integer. 
+
+var convertToNumber = function (stringFormat) {
+var stringFormat = prompt("Pick a number","")
+var answer;
+answer = parseInt(stringFormat);
+return answer;
+};
+
+console.log(convertToNumber())
+
+console.log(isNaN(convertToNumber))
+
+//Number function 2
+
+
