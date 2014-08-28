3f049ac8cc7a1add98ddb88e3d4a31fa523cee09
cobyrpilpel <cobyrpilpel@Cobys-MacBook-Pro.local>
2014-08-28 17:23:33 -0400

    Formatting corrections for the first function. In all of my editing I forgot to re-indent and re-organize the code block. No functional changes made.

--- rename-me.js
+++ rename-me.js
@@ -6,21 +6,26 @@
 //Aug 28th 2014
 
 //Global variables (I'm throwing these up here as I run into a situation where I need one)
-
+//Number function 1 global variables
+//none needed
+//Number function 2 global variables
 
 //Number function 1 - Converting a string to an integer. 
 
-var convertToNumber = function (stringFormat) {
-var stringFormat = prompt("Pick a number","")
-var answer;
-answer = parseInt(stringFormat);
-return answer;
-};
+var convertToNumber = function (stringFormat) 
+	{var stringFormat = prompt("Pick a number","")
+	var answer;
+	answer = parseInt(stringFormat);
+	return answer;};
 
 console.log(convertToNumber())
 
 console.log(isNaN(convertToNumber))
 
-//Number function 2
+//Number function 2 - Format a number to use a specific number of decimal places
+
+
+
+
 
 
